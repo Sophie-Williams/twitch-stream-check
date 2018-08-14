71efa61c8d07a0f04c9b02dbe8afd6fbca1cb706
Joel Eisner <jeisner93@gmail.com>
2018-08-14 18:40:08 -0400

    Updated the entry script to organize flags alphabetically

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const action = require('./action'),
 program
     .version(pkg.version)
     .arguments('<twitch-username>')
-    .option('-g, --game',      'The game the streamer is playing')
-    .option('-v, --viewers',   'The amount of viewers the streamer currently has watching')
-    .option('-s, --started',   'When the streamer went live')
-    .option('-m, --mature',    'Whether the streamer streams mature content or not')
-    .option('-t, --title',     'The current stream title')
-    .option('-p, --partnered', 'Whether the streamer is partnered or not')
     .option('-f, --followers', 'The amount of followers the streamer has')
+    .option('-g, --game',      'The game the streamer is playing')
+    .option('-m, --mature',    'Whether the streamer streams mature content or not')
+    .option('-p, --partnered', 'Whether the streamer is partnered or not')
+    .option('-s, --started',   'When the streamer went live')
+    .option('-t, --title',     'The current stream title')
+    .option('-v, --viewers',   'The amount of viewers the streamer currently has watching')
     .action(action)
     .parse(process.argv);
